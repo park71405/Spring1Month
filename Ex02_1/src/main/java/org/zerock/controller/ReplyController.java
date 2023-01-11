122c9f35a36096ab8b3a48b03158f9b645048250
park71403 <park71403@gmail.com>
2023-01-11 16:43:45 +0900

    ajax 댓글 처리 시도 중

--- Ex02_1/src/main/java/org/zerock/controller/ReplyController.java
+++ Ex02_1/src/main/java/org/zerock/controller/ReplyController.java
@@ -5,13 +5,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
 import lombok.AllArgsConstructor;
 
-@RestController("/replies/")
+@RestController()
+@RequestMapping("/replies/")
 @AllArgsConstructor
 public class ReplyController {
 
